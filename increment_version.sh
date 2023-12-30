61f6e5fae7772392dd7f2c46f2c30e2eda5e4f8c
Ice Tee <amit.tiwari@gmail.com>
2023-12-30 14:16:26 -0600

    deleted file_*.txt

--- increment_version.sh
+++ increment_version.sh
@@ -8,8 +8,20 @@ filename="file_$timestamp.txt"
 # Use the filename in your script or command
 echo "Creating $filename..."
 touch $filename
+
+echo "git config emails..."
+git config --global user.email "amit.tiwari@gmail.com"
+echo "git config user name..."
+git config --global user.name "Ice Tee"
+echo "git status..."
+git status
+
 echo "Listing files..."
 ls -latr
+
+echo "git diff files..."
+git diff
+
 # Other commands using the filename can go here
 # For example, you might want to write data to the file or perform other operations.
 
@@ -20,10 +32,8 @@ ls -latr
 #    echo "Git status failed or produced unexpected output"
 #    exit 1
 #fi 
-git config --global user.email "amit.tiwari@gmail.com"
-git config --global user.name "Ice Tee"
-git status
-
-git add ./$filename
-git commit -m "bot added ${filename}"
+echo "gonna run: git add ./${filename}"
+git add ${filename}
+echo "gonna run: git commit -m \"bot added\""
+git commit -m "bot added"
 echo "Done."
