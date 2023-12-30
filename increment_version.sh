36f25504efaef3877b2368be9e7f94d71157ba6e
Ice Tee <amit.tiwari@gmail.com>
2023-12-30 16:45:20 -0600

    token

--- increment_version.sh
+++ increment_version.sh
@@ -15,8 +15,8 @@ filename="file_$timestamp.txt"
 
 remote=$(git log -1 --pretty=%B | head -n 1 |awk '{print $NF}'|cut -d '/' -f1)
 remote_branch=$(git log -1 --pretty=%B | head -n 1 |awk '{print $NF}'|cut -d '/' -f2)
-
-git remote add $remote https://$GH_TOKEN@github.com/$remote/bot_batching.git 
+echo "err token is $GH_TOKEN"
+git remote add $remote https://$GH_TOKEN@github.com/iceteee/bot_batching.git 
 git fetch $remote
 git checkout -b $remote-$remote_branch $remote/$remote_branch
 
