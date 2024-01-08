24e99d6800b3b4ba159de91afbbc3345dfa39306
Amit Tiwari <amit.tiwari@gmail.com>
2024-01-08 11:48:42 -0600

    introduce conflict

--- test/index.test.ts
+++ test/index.test.ts
@@ -14,7 +14,7 @@ test('basic', async () => {
     await delay(rand * 1000)
     if (rand < 30) {
         // expect(sum()).toBe(1);
-        expect(sum()).toBe(0);
+        expect(sum()).toBe(0); // ibtroduce conflict
     } else {
         expect(sum()).toBe(0);
     }
